880211a6b765ff4e444dd1439f91cd84222f3b74
Deepson <ds6546@truman.edu>
2020-10-25 01:37:40 -0500

    test push

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// Team truman
 import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
@@ -12,29 +13,27 @@ import ContactTracer from "./components/ContactTracer";
 import HealthChecker from "./components/HealthChecker";
 import EmployerSchedule from "./components/EmployerSchedule";
 import EmployeeSchedule from "./components/EmployeeSchedule";
-import Availability from './components/Availability';
+import Availability from "./components/Availability";
 import Signout from "./components/Signout";
 
-
 function App() {
-
   return (
     <Router>
-      <div className="App">
-        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-          <div className="container">
-            <Link className="navbar-brand" to={"/sign-in"}>
+      <div className='App'>
+        <nav className='navbar navbar-expand-lg navbar-light fixed-top'>
+          <div className='container'>
+            <Link className='navbar-brand' to={"/sign-in"}>
               The Wellness Scheduler
             </Link>
-            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <Link className="nav-link" to={"/sign-in"}>
+            <div className='collapse navbar-collapse' id='navbarTogglerDemo02'>
+              <ul className='navbar-nav ml-auto'>
+                <li className='nav-item'>
+                  <Link className='nav-link' to={"/sign-in"}>
                     Login
                   </Link>
                 </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to={"/sign-up"}>
+                <li className='nav-item'>
+                  <Link className='nav-link' to={"/sign-up"}>
                     Sign up
                   </Link>
                 </li>
@@ -42,25 +41,29 @@ function App() {
             </div>
           </div>
         </nav>
-          <div className="auth-wrapper">
-            <div className="auth-inner">
-              <Switch>
-                <Route exact path="/" component={Login} />
-                <Route path="/sign-in" component={Login} />
-                <Route path="/sign-up" component={SignUp} />
-                <Route exact path="/employer" component={EmployerHome} />
-                <Route exact path="/employee" component={EmployeeHome} />
-                <Route exact path="/employer/contact-tracer" component={ContactTracer}/>
-                <Route path="/employee/healthchecker" component={HealthChecker}/>
-                <Route path="/employer/schedule" component={EmployerSchedule} />
-                <Route path="/employee/schedule" component={EmployeeSchedule} />
-                <Route path="/employer/tracer" component={ContactTracer} />
-                <Route path="/employee/availability" component={Availability} />
-                <Route path="/signout" component={Signout} />
-                <Route path="*" component={Error} />
-              </Switch>
-            </div>
+        <div className='auth-wrapper'>
+          <div className='auth-inner'>
+            <Switch>
+              <Route exact path='/' component={Login} />
+              <Route path='/sign-in' component={Login} />
+              <Route path='/sign-up' component={SignUp} />
+              <Route exact path='/employer' component={EmployerHome} />
+              <Route exact path='/employee' component={EmployeeHome} />
+              <Route
+                exact
+                path='/employer/contact-tracer'
+                component={ContactTracer}
+              />
+              <Route path='/employee/healthchecker' component={HealthChecker} />
+              <Route path='/employer/schedule' component={EmployerSchedule} />
+              <Route path='/employee/schedule' component={EmployeeSchedule} />
+              <Route path='/employer/tracer' component={ContactTracer} />
+              <Route path='/employee/availability' component={Availability} />
+              <Route path='/signout' component={Signout} />
+              <Route path='*' component={Error} />
+            </Switch>
           </div>
+        </div>
       </div>
     </Router>
   );
